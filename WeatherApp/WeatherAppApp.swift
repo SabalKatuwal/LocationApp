c91e5ea6a877b4d3e389f490449276cc5e799857
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-07-24 18:17:26 +0545

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Sabal on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
